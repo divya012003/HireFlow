9f1e475fd6677282c866d5fe12a0b74ff6fafa3e
omkar5454more <divyamote2003@gmail.com>
2026-03-09 13:21:17 +0530

    fix cors trailing slash

--- Frontend/src/components/services/api.jsx
+++ Frontend/src/components/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"https://hireflow-1-gtpy.onrender.com"
+    baseURL:"https://hireflow-1-gtpy.onrender.com/api"
 })
 
 api.interceptors.request.use(
